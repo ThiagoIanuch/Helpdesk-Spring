912db33f7d91ce1099f2c558978a8820a4d5d8d3
ThiagoIanuch <thiagoianuch@hotmail.com>
2024-10-06 15:31:53 -0300

    Criar conexão com o banco com anotações JPA

--- src/main/java/com/thiagoianuch/helpdesk/domain/Cliente.java
+++ src/main/java/com/thiagoianuch/helpdesk/domain/Cliente.java
@@ -1,11 +1,20 @@
 package com.thiagoianuch.helpdesk.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import com.thiagoianuch.helpdesk.domain.enums.Perfil;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Cliente extends Pessoa{
 	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
@@ -15,5 +24,13 @@ public class Cliente extends Pessoa{
 	public Cliente(Integer id, String nome, String cpf, String email, String senha, Set<Integer> perfis) {
 		super(id, nome, cpf, email, senha, perfis); 
 		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
 	}	
 }
